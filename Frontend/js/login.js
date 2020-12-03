a871dfda7033e5d20f99dbfed3ed0d1755bac3f0
soumyajit4419 <ashish454570@gmail.com>
2020-12-04 00:38:15 +0530

    ui fixes and ajax

---
+++ Frontend/js/login.js
@@ -0,0 +1,67 @@
+$("#errorAlert").hide();
+
+const url = "http://127.0.0.1:3600";
+
+function login() {
+  var userType = $("#inputUser").val();
+  var email = $("#email").val();
+  var password = $("#password").val();
+
+  if (userType == "") return;
+  if (email == "") return;
+  if (password == "") return;
+
+  if (userType == "Doc") {
+    $.ajax({
+      url: url + "/auth/loginDoc",
+      method: "POST",
+      crossDomain: true,
+      data: {
+        email: email,
+        password: password,
+      },
+      success: function (res) {
+        if (res.status !== 200) {
+          $("#errorAlert").text(res.message);
+          $("#errorAlert").show();
+          setTimeout(function () {
+            window.location.reload();
+          }, 2000);
+        } else if (res.status === 200) {
+          localStorage.setItem("token", res.token);
+          window.location = "doctor/docDash.html";
+        }
+      },
+      error: function (err) {
+        console.log(err);
+        alert(err);
+      },
+    });
+  } else if (userType == "Pat") {
+    $.ajax({
+      url: url + "/auth/loginUser",
+      method: "POST",
+      crossDomain: true,
+      data: {
+        email: email,
+        password: password,
+      },
+      success: function (res) {
+        if (res.status !== 200) {
+          $("#errorAlert").text(res.message);
+          $("#errorAlert").show();
+          setTimeout(function () {
+            window.location.reload();
+          }, 2000);
+        } else if (res.status === 200) {
+          localStorage.setItem("token", res.token);
+          window.location = "patient/patDash.html";
+        }
+      },
+      error: function (err) {
+        console.log(err);
+        alert(err);
+      },
+    });
+  }
+}
---
+++ Frontend/js/register.js
@@ -0,0 +1,95 @@
+$("#successAlert").hide();
+$("#errorAlert").hide();
+$("#successBtn").hide();
+
+const url = "http://127.0.0.1:3600";
+
+function register() {
+  var name = $("#name").val();
+  var email = $("#email").val();
+  var password = $("#password").val();
+  var confirmPassword = $("#confirmPassword").val();
+  var age = $("#age").val();
+  var sex = $("#sex").val();
+  var userType = $("#inputUser").val();
+
+  if (name == "") return;
+  if (email == "") return;
+  if (password == "") return;
+  if (confirmPassword == "") return;
+  if (age == "") return;
+  if (sex == "") return;
+  if (userType == "") return;
+
+  if (password != confirmPassword) {
+    $("#errorAlert").text("Password Din't Match");
+    $("#errorAlert").show();
+    setTimeout(function () {
+      window.location.reload();
+    }, 2000);
+  }
+
+  if (userType == "Doc") {
+    $.ajax({
+      url: url + "/auth/registerDoc",
+      method: "POST",
+      crossDomain: true,
+      data: {
+        name: name,
+        email: email,
+        password: password,
+        confirmPassword: confirmPassword,
+        age: age,
+        sex: sex,
+      },
+      success: function (res) {
+        if (res.status !== 200) {
+          $("#errorAlert").text(res.message);
+          $("#errorAlert").show();
+          setTimeout(function () {
+            window.location.reload();
+          }, 2000);
+        } else if (res.status === 200) {
+          $("#successAlert").text(res.message);
+          $("#successAlert").show();
+          $("#successBtn").show();
+        }
+      },
+      error: function (err) {
+        console.log(err);
+        alert(err);
+      },
+    });
+  } else if (userType == "Pat") {
+    $.ajax({
+      url: url + "/auth/registerUser",
+      method: "POST",
+      crossDomain: true,
+      data: {
+        name: name,
+        email: email,
+        password: password,
+        confirmPassword: confirmPassword,
+        age: age,
+        sex: sex,
+      },
+      success: function (res) {
+        if (res.status !== 200) {
+          $("#errorAlert").text(res.message);
+          $("#errorAlert").show();
+          setTimeout(function () {
+            window.location.reload();
+          }, 2000);
+        } else if (res.status === 200) {
+          $("#successAlert").text(res.message);
+          $("#successAlert").show();
+          $("#successBtn").show();
+        }
+      },
+      error: function (err) {
+        console.log(err);
+        alert(err);
+      },
+    });
+  }
+}
