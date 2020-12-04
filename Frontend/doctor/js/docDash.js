c4e672d662e9146586b24f5f44e1a6499b1fcbdb
soumyajit4419 <ashish454570@gmail.com>
2020-12-04 10:15:49 +0530

    upadted ajax and verify all pages

---
+++ Frontend/doctor/js/docDash.js
@@ -0,0 +1,22 @@
+const url = "http://127.0.0.1:3600";
+
+$.ajax({
+  url: url + "/doc/verify",
+  method: "GET",
+  crossDomain: true,
+  headers: {
+    "x-access-token": localStorage.getItem("token"),
+  },
+  success: function (res) {
+    if (res.status !== 200) {
+      window.location = "../500.html";
+    } else if (res.status === 200) {
+      console.log("success");
+    }
+  },
+  error: function (err) {
+    console.log(err);
+    alert(err);
+  },
+});
+
---
+++ Frontend/patient/js/patDash.js
@@ -0,0 +1,41 @@
+const url = "http://127.0.0.1:3600";
+
+$.ajax({
+  url: url + "/user/verify",
+  method: "GET",
+  crossDomain: true,
+  headers: {
+    "x-access-token": localStorage.getItem("token"),
+  },
+  success: function (res) {
+    if (res.status !== 200) {
+      window.location = "../500.html";
+    } else if (res.status === 200) {
+      console.log("success");
+    }
+  },
+  error: function (err) {
+    console.log(err);
+    alert(err);
+  },
+});
+
+$.ajax({
+  url: url + "/user/getUserData",
+  method: "GET",
+  crossDomain: true,
+  headers: {
+    "x-access-token": localStorage.getItem("token"),
+  },
+  success: function (res) {
+    if (res.status !== 200) {
+      window.location = "../500.html";
+    } else if (res.status === 200) {
+      console.log(res.data);
+    }
+  },
+  error: function (err) {
+    console.log(err);
+    alert(err);
+  },
+});
