575fd76f5f85b26ad968531be196518ae5620dba
soumyajit4419 <ashish454570@gmail.com>
2020-12-08 00:37:25 +0530

    cors error and not working api

--- Healthcare Models/app.py
+++ Healthcare Models/app.py
@@ -6,31 +6,29 @@ import json
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'rahulkp'
-app.config['CORS_HEADERS'] = 'Content-Type'
 cors = CORS(app)
 
 model = pickle.load(open('model1.pkl', 'rb'))
 
 
-@app.route('/predict',methods=['POST'])
-@cross_origin()
+@app.route('/predict', methods=['POST'])
 def predict():
 
     data = request.get_json()
-    
-    new_list=data["values"]
-    
+
+    new_list = data["values"]
+
     int_features = [int(x) for x in new_list]
-    
+
     final_features = [np.array(int_features)]
-    
+
     prediction = model.predict(final_features)
 
     output = prediction[0]
- 
+
     return jsonify(output)
 
 
 if __name__ == "__main__":
+    print("starting flask server")
     app.run(debug=True)
